907e75b37c8e6856307f169936b44b47a7ab7318
Sean DiGirolamo <s.digirolamo218@gmail.com>
2021-09-08 14:16:10 -0400

    Added terminus nerdfont as fallback font

--- config.h
+++ config.h
@@ -6,7 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Terminus:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "Awesome:pixelsize=14:antialias=true:autohint=true"};
+//static char *font2[] = { "Hack Regular Nerd Font Complete Mono:pixelsize=14:antialias=true:autohint=true"};
+static char *font2[] = { "TerminessTTF Nerd Font:pixelsize=14:antialias=true:autohint=true"};
 static int borderpx = 2;
 
 /*
